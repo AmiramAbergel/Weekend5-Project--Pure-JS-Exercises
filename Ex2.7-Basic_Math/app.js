c3e507c355da67a60197b1a700e52132aadc1900
unknown <Amiram.ab@gmail.com>
2022-10-06 20:17:04 +0300

    2.7 Done

--- Ex2.7-Basic_Math/app.js
+++ Ex2.7-Basic_Math/app.js
@@ -9,3 +9,24 @@ basicOp('-', 15, 18) // Output: -3
 basicOp('*', 5, 5) // Output: 25
 basicOp('/', 49, 7) // Output: 7
 */
+
+const basicOp = (operation, value1, value2) => {
+    switch (operation) {
+        case "+":
+            return value1 + value2;
+        case "-":
+            return value1 - value2;
+        case "*":
+            return value1 * value2;
+        case "/":
+            return value1 / value2;
+        default:
+            return `Unsupported operator: ${operation}`;
+    }
+};
+
+//Test: Ex2.7
+basicOp("+", 4, 7); // Output: 11
+basicOp("-", 15, 18); // Output: -3
+basicOp("*", 5, 5); // Output: 25
+basicOp("/", 49, 7); // Output: 7
