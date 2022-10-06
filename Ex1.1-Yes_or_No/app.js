907f167b0f6d652063195710c365920317fb1ca3
unknown <Amiram.ab@gmail.com>
2022-10-06 17:44:17 +0300

    1.1 Done

--- Ex1.1-Yes_or_No/app.js
+++ Ex1.1-Yes_or_No/app.js
@@ -3,3 +3,11 @@
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
 string for false.
 */
+
+const isStr = (bool) => {
+    return bool === true ? "Yes" : "No";
+};
+
+//Test Ex1.1
+isStr(true); // "Yes"
+isStr(false); // "No"
