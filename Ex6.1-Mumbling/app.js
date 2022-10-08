c11fb02dbbb471be7fce63b2aa638bb48b011dfa
unknown <Amiram.ab@gmail.com>
2022-10-08 18:09:27 +0300

    6.1 Done

--- Ex6.1-Mumbling/app.js
+++ Ex6.1-Mumbling/app.js
@@ -3,14 +3,21 @@
 This time no story, no theory. The examples below show you how to write function accum :
 Examples:
 accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeeezzzz-Tttttt-Yyyyyyy"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z .
 */
 
-const accum = () => {};
+const accum = (str) => {
+    let upper = str.toUpperCase();
+    let strArray = upper.split("");
+    for (let i = 0; i < strArray.length; i++) {
+        strArray[i] += strArray[i].toLowerCase().repeat(i);
+    }
+    return strArray.join("-");
+};
 
 // Test Ex6.1
-accum("abcd"); // Output:
-accum("RqaEzty"); // Output:
-accum("cwAt"); // Output:
+accum("abcd"); // Output: 'A-Bb-Ccc-Dddd'
+accum("RqaEzty"); // Output: 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'
+accum("cwAt"); // Output: 'C-Ww-Aaa-Tttt'
