984e91ca2029820fc6c2e3786970e12e64ac63ba
unknown <Amiram.ab@gmail.com>
2022-10-09 15:43:43 +0300

    5.4 Done (Efficiencies can be improved)

--- Ex5.4-To_weird_case/app.js
+++ Ex5.4-To_weird_case/app.js
@@ -13,8 +13,20 @@ toWeirdCase( "String" );//=> returns "StRiNg"
 toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
 */
 
-const toWeirdCase = () => {};
+const toWeirdCase = (str) => {
+    let res = str.toLowerCase().split(" ");
+    for (let i = 0; i < res.length; i++) {
+        res[i] = res[i].split("");
+        for (let j = 0; j < res[i].length; j++) {
+            if (j % 2 === 0) {
+                res[i][j] = res[i][j].toUpperCase();
+            }
+        }
+        res[i] = res[i].join("");
+    }
+    return res.join(" ");
+};
 
 //Test: Ex5.4
-toWeirdCase("String"); // Output:
-toWeirdCase("Weird string case"); // Output:
+toWeirdCase("String"); // Output: 'StRiNg'
+toWeirdCase("Weird string case"); // Output: 'WeIrD StRiNg CaSe'
