dd20bc910acfc3dd3fd1c142a45c6afa0abe5b5c
unknown <Amiram.ab@gmail.com>
2022-10-09 17:13:58 +0300

    4.2 Done

--- Ex4.2-Tribonacci/app.js
+++ Ex4.2-Tribonacci/app.js
@@ -21,13 +21,41 @@ clearly specified ;)
 */
 
 /**
+ * @param {array} signature
  * @param {number} n
- * @return {number}
+ * @return {array}
  */
-const tribonacci = function (n) {};
+function tribonacci(signature, n) {
+    let result = [];
+    if (n === 0) {
+        return result;
+    }
+    if (n === 1) {
+        result.push(signature[0]);
+        return result;
+    }
+    if (n === 2) {
+        result.push(signature[0]);
+        result.push(signature[1]);
+        return result;
+    }
+    if (n === 3) {
+        result.push(signature[0]);
+        result.push(signature[1]);
+        result.push(signature[2]);
+        return result;
+    }
+    result.push(signature[0]);
+    result.push(signature[1]);
+    result.push(signature[2]);
+    for (let i = 3; i < n; i++) {
+        result.push(result[i - 1] + result[i - 2] + result[i - 3]);
+    }
+    return result;
+}
 
 // Test: Ex4.2
-tribonacci(2); // Output:
-tribonacci(3); // Output:
-tribonacci(4); // Output:
-tribonacci(5); // Output:
+tribonacci([0, 1, 1], 10); // Output: [0, 1, 1, 2, 4, 7, 13, 24, 44, 81]
+tribonacci([1, 1, 1], 1); // Output: [1]
+tribonacci([300, 200, 100], 0); // Output: []
+tribonacci([0.5, 0.5, 0.5], 30); // Output: [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5]
