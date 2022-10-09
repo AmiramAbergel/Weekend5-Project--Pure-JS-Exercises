a2cc11af98fb1876c4e091167a4fc5eb4348897c
unknown <Amiram.ab@gmail.com>
2022-10-09 16:56:37 +0300

    Extra ex7 Done

---
+++ Ex7-Implement_Functionality/app.js
@@ -0,0 +1,37 @@
+/*
+Ex7 - Implement The Following JS Methods -
+Implement the following methods -
+- Filter
+- ForEach
+- Map
+Using only for(), array and objects (without other js methods)
+Find Bugs \ Debug Code
+Bug !!!!!
+*/
+
+// Filter
+const myFilter = (callback) => {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+};
+
+// ForEach
+const myForEach = (callback) => {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+};
+
+// Map
+const myMap = (callback) => {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+        result.push(callback(this[i], i, this));
+    }
+    return result;
+};
